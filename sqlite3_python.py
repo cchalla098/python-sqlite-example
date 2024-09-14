96086cc798fb4d3606277687af2e2710e1709d61
cchalla098 <144577310+cchalla098@users.noreply.github.com>
2024-09-14 09:25:45 -0400

    Create sqlite3_python.py

---
+++ sqlite3_python.py
@@ -0,0 +1,16 @@
+import sqlite3
+connection = sqlite3.connect('example.db')
+cursor= connection.cursor()
+cursor.create("CREATE TABLE students (id INTEGER PRIMARY KEY, name TEXT, grade REAL)")
+cursor.execute("INSERT INTO students (name,grade) VALUES ('ALICE', 85.5)")
+cursor.execute("SELECT * FROM students")
+rows= cursor.fetchall()
+for rows in rows:
+  print (row)
+connection.commit()
+connection.close()
+try:
+cursor.execute("SELECT * FROM non_existing_table")
+except sqlite3.OperationalError as e:
+print(f"An error occurred: {e}")
+cursor.execute("INSERT INTO students (name, grade) VALUES (?, ?)", ('Bob', 92.3))
